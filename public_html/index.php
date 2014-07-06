fceb85539d10b102b57ce57d08438b1a7d461e9b
nghiatd@smartosc.com <nghia-ubuntu>
2014-07-06 18:25:21 +0700

    edit from ubuntu

--- public_html/index.php
+++ public_html/index.php
@@ -3,7 +3,7 @@
 // ob_start ( "ob_gzhandler" );
 require realpath ( '../application' ) . '/init.php';
 /**
- * Zend_Application
+ * Zend_Application tdb
  */
 require_once 'Zend/Application.php';
 $application = new Zend_Application ( APPLICATION_ENV, APPLICATION_PATH . 'configs/application.ini' );
